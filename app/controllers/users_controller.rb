8a1f69715b1bda19716c8ef15c14a0b38931d963
ds Ears <info@dspheretech.com>
2016-03-23 15:16:10 +0000

    Implemented the friends platform and linked with users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
-  
+
   def my_finance
     @user_stocks = current_user.stocks
-
     @user = current_user
   end
-  
+
+  def my_friends
+
+  end
 end
-  
